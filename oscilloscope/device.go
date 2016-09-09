2575c183a2f400751a828e3b66d505191f371ef3
Sebastian Zagrodzki <sebek@zagrodzki.net>
2016-09-09 23:03:21 +0200

    Add comments.

--- oscilloscope/device.go
+++ oscilloscope/device.go
@@ -7,20 +7,27 @@ import (
 	"time"
 )
 
+// ChanID represents the ID of a probe channel on a scope.
 type ChanID string
 
-type Channel interface {
-	ID() ChanID
-	SetVoltRange(float64) error
-}
-
+// VoltRange represents a measure range in Volts.
 type VoltRange float64
 
+// String returns a human-readable representation of measurement range.
 func (v VoltRange) String() {
 	return fmt.Sprintf("+-%fV", v)
 }
 
-type SampleRate int
+// Channel represents the probe channel on a scope.
+type Channel interface {
+	// ID returns the channel ID
+	ID() ChanID
+
+	// GetVoltRanges returns a slice with available ranges that can be passed to SetVoltRange.
+	GetVoltRanges() []VoltRange
+	// SetVoltRange adjusts the sensitivity
+	SetVoltRange(VoltRange) error
+}
 
 func fmtVal(v float64) string {
 	av := math.Abs(v)
@@ -46,14 +53,30 @@ func fmtVal(v float64) string {
 	return fmt.Sprintf("%s%s", ret, sfx)
 }
 
+// SampleRate represents a Device sampling frequency in samples/second.
+type SampleRate int
+
+// String returns a human-readable representation of sampling rate.
 func (s SampleRate) String() {
 	return fmt.Sprintf("%s samples/s", fntVal(float64(s)))
 }
 
+// Device represents a connected sampling device (e.g. USB oscilloscope),
 type Device interface {
+	// String returns a description of the device. It should be specific enough
+	// to allow the user to identify the physical device that this value
+	// represents.
 	String() string
+
+	// Channels returns a map of Channels indexed by their IDs. Channel can be used
+	// to configure parameters related to a single capture source.
 	Channels() map[ChanID]Channel
-	StartCapture() error
-	StopCapture() error
+
+	// ReadData asks the device for a trace.
+	// This interface assumes all channels on a single Device are sampled at the
+	// same rate and return the same number of samples for every run.
 	ReadData() (map[ChanID][]byte, time.Duration, error)
+
+	// GetSampleRates returns a slice of sample rates available on this device.
+	GetSampleRates() []SampleRate
 }
