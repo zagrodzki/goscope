f924271755338c90610f7da950591ffac9fd0ed8
Sebastian Zagrodzki <sebek@zagrodzki.net>
2016-09-21 02:45:46 +0200

    in circle.yml use the scripts in build/ rather than inline scripts, more readable that way.

--- build/test-and-coverage.sh
+++ build/test-and-coverage.sh
@@ -3,7 +3,7 @@
 # writing coverage profiles for multiple packages into a single file.
 echo "" > coverage.txt
 RET=0
-for d in $(go list "$IMPORT_PATH/..." | grep -v vendor); do
+for d in $(go list "${IMPORT_PATH}/..." | grep -v vendor); do
   go test -race -coverprofile=profile.out -covermode=atomic $d
   STATUS=$?
   if [ $STATUS -ne 0 ]; then
