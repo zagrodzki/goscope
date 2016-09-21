472c4908e896f0b0d6dba73b3e446e74399459f8
Sebastian Zagrodzki <sebek@zagrodzki.net>
2016-09-21 02:54:40 +0200

    add go-junit-report to report test results in a way that CircleCI can parse.

--- build/test-and-coverage.sh
+++ build/test-and-coverage.sh
@@ -4,11 +4,13 @@
 echo "" > coverage.txt
 RET=0
 for d in $(go list "${IMPORT_PATH}/..." | grep -v vendor); do
-  go test -race -coverprofile=profile.out -covermode=atomic $d
+  go test -race -coverprofile=profile.out -covermode=atomic -v $d |\
+          tee report-golang.txt
   STATUS=$?
   if [ $STATUS -ne 0 ]; then
     RET=$STATUS
   fi
+  go-junit-report < report-golang.txt > report.xml
   if [ -f profile.out ]; then
     cat profile.out >> coverage.txt
     rm profile.out
