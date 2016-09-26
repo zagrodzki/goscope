fd2e1d1290f11012d5b5c0e83932029d59b59b7c
Sebastian Zagrodzki <sebek@zagrodzki.net>
2016-09-26 23:35:15 +0200

    Use /bin/bash (/bin/sh behavior is different from bash when handling "-e" cmdline flag). More verbose error message.

--- build/gofmt.sh
+++ build/gofmt.sh
@@ -1,6 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 FILES="$( find . -name '*.go' -print0 | xargs -0 gofmt -l )"
 if [ -n "$FILES" ]; then
-    echo -e "go fmt diff in:\n$FILES"
+    echo "go fmt diff found."
+    echo "Run the following command in the git repo root to fix the formatting:"
+    echo "gofmt -w $( echo "$FILES" | tr '\n' ' ' )"
     exit 1
 fi
