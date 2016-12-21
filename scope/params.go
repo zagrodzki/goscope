b77e21f3b627021643a346a6ec97e688a0cddbe0
Sebastian Zagrodzki <sebek@zagrodzki.net>
2016-12-21 16:57:51 -0500

    Fix comment format.

--- scope/params.go
+++ scope/params.go
@@ -14,7 +14,7 @@
 
 package scope
 
-// A control element of the Device - a button, knob, switch on the scope front panel.
+// Param is a control element of the Device - a button, knob, switch on the scope front panel.
 type Param interface {
 	// Name returns the name of the param, intended for the UI.
 	Name() string
@@ -24,17 +24,20 @@ type Param interface {
 	Set(string) error
 }
 
-// Anything that would be represented by a button or a discrete knob/slider. This control
-// represents discrete settings that don’t have an ordering - e.g. name, type etc.
-// or a range with not many discrete values (e.g. time base length). For ranges with many
-// values (unsuitable for a drop-down list), a RangeParam is better.
+// SelectParam represents anything that would be a button or a discrete
+// knob/slider on a physical device. This control represents discrete settings
+// that don’t have an ordering - e.g. name, type etc. or a range with
+// few discrete values (e.g. time base length).
+// For ranges with many values (unsuitable for a drop-down list),
+// a RangeParam is better.
 type SelectParam interface {
 	Param
 	// Values returns the list of available values to choose from.
 	Values() []string
 }
 
-// A continuous control knob/slider. The speed of change should accelerate when the user
+// RangeParam represents a continuous control knob/slider.
+// The speed of change may accelerate when the user
 // keeps increasing the value at a quick steady pace.
 type RangeParam interface {
 	Param
